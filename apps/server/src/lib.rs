2a6118730feea4493f1900ab26f16ff49d9646a4
traversalnat <1914619958@qq.com>
2022-10-17 15:25:41 +0800

    能够与本地 192.168.31.xx 通信（本机IP）通信，但是接收不到局域网其他电脑的arp包。 连接服务器时，能接收到SYN包，但无法建立连接。

--- apps/server/src/lib.rs
+++ apps/server/src/lib.rs
@@ -8,31 +8,32 @@ use stdio::*;
 pub fn app_main() {
     let sender = sys_sock_create();
     let receiver = sys_sock_create();
-    let remote_endpoint = IpEndpoint::new(IpAddress::v4(10, 42, 117, 199), 6000);
+    let remote_endpoint = IpEndpoint::new(IpAddress::v4(192, 168, 31, 156), 6000);
+    // let remote_endpoint = IpEndpoint::new(IpAddress::v4(49, 235, 113, 66), 6000);
     sys_sock_listen(sender, 6000).unwrap();
     if let Ok(_) = sys_sock_connect(receiver, remote_endpoint) {};
     println!("connected");
 
     unsafe {
-            let mut tx: String = "hello, world".to_owned();
-            let mut rx = vec![0 as u8; tx.len()];
+        let mut tx: String = "hello, world".to_owned();
+        let mut rx = vec![0 as u8; tx.len()];
 
-            println!("read status");
-            while !sys_sock_status(sender).can_send {
-                // println!("cannot send!");
-            }
-            println!("sending");
-            if let Some(size) = sys_sock_send(sender, tx.as_bytes_mut()) {
-                println!("send {size} words");
-            }
+        println!("read status");
+        while !sys_sock_status(receiver).can_send {
+            // println!("cannot send!");
+        }
+        println!("sending");
+        if let Some(size) = sys_sock_send(receiver, tx.as_bytes_mut()) {
+            println!("send {size} words");
+        }
 
-            while !sys_sock_status(receiver).can_recv {
-                // println!("cannot recv");
-            }
-            println!("recving");
-            if let Some(size) = sys_sock_recv(receiver, rx.as_mut_slice()) {
-                println!("receive {size} words");
-            }
-            println!("{}", core::str::from_utf8_unchecked(&rx));
+        while !sys_sock_status(receiver).can_recv {
+            // println!("cannot recv");
+        }
+        println!("recving");
+        if let Some(size) = sys_sock_recv(receiver, rx.as_mut_slice()) {
+            println!("receive {size} words");
+        }
+
     };
 }
