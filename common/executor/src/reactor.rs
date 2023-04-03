9b78f6aa607d1ceb2d628b4bbcd2c0c862ba393b
traversalnat <1914619958@qq.com>
2023-04-03 21:11:45 +0800

    add irq reactor for common/executor

---
+++ common/executor/src/reactor.rs
@@ -0,0 +1,46 @@
+use crate::EXECUTOR;
+use core::{
+    future::{poll_fn, Future},
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+#[derive(Clone, Copy)]
+pub enum IRQ {
+    UART0_IRQ,
+    VIRTIO0_IRQ,
+    E1000_IRQ,
+}
+
+struct IRQ_EVENT {
+    irq: IRQ,
+    ready: bool,
+}
+
+impl IRQ_EVENT {
+    pub fn new(irq: IRQ) -> Self {
+        Self {
+            irq: irq,
+            ready: false,
+        }
+    }
+}
+
+impl Future for IRQ_EVENT {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let irq = self.irq;
+        if self.ready {
+            Poll::Ready(())
+        } else {
+            self.get_mut().ready = true;
+            EXECUTOR.wait().sys_register_irq(cx, irq);
+            Poll::Pending
+        }
+    }
+}
+
+pub async fn async_wait_irq(irq: IRQ) {
+    IRQ_EVENT::new(irq).await
+}
