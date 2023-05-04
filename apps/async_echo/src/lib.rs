e20c0e6ad602c86ddc30ed7c037c7aa3b910df98
Hi9 <1914619958@qq.com>
2023-05-04 23:25:32 +0800

    fix bug

--- apps/async_echo/src/lib.rs
+++ apps/async_echo/src/lib.rs
@@ -1,26 +1,59 @@
 #![no_std]
-use executor::{Runner, async_yield};
-use stdio::*;
+extern crate alloc;
+
+use core::time::Duration;
+use executor::{async_yield, async_wait, async_block_on};
 use spin::Lazy;
+use stdio::{*, log::info};
+use thread::spawn;
+
+use alloc::boxed::Box;
 
 pub async fn app_main() {
-    println!("async echo");
+    use mpmc::channel;
+    use core::{future::Future, pin::Pin};
+    use core::hint::spin_loop;
 
-    static EX: Lazy<Runner> = Lazy::new(|| Runner::new());
+    type AsyncFuture = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
 
-    EX.block_on(async {
-        EX.spawn(async {
+    let (tx, rx) = channel::<AsyncFuture>();
+
+    for _ in 0..8 {
+        let q1 = rx.clone();
+        spawn(async move {
             loop {
-                print!("hello ");
-                async_yield().await;
+                match q1.recv() {
+                    Ok(task) => {
+                        async_block_on(task);
+                    },
+                    _ => spin_loop(),
+                }
             }
-        });
+        }, true);
+    }
 
-        EX.spawn(async {
-            loop {
-                print!("world\n");
-                async_yield().await;
-            }
-        });
-    });
+    for i in 0..100 {
+        tx.send(Box::pin(say_hi(i))).unwrap();
+        tx.send(Box::pin(say_goodbye(i))).unwrap();
+    }
+
+    loop {
+        async_yield().await;
+    }
+}
+
+fn fb(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+    return fb(n - 1) + fb(n - 2);
+}
+
+async fn say_hi(index: u32) {
+    fb(32);
+    println!("hi {index}");
+}
+
+async fn say_goodbye(index: u32) {
+    println!("goodbye {index}");
 }
